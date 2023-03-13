4d189f2ea19c2d7257db49a7f20a1af0712de5f1
MohaElder169 <calen0909@hotmail.com>
2023-03-13 15:17:04 +0700

    remove unused stuff

--- app/app.js
+++ app/app.js
@@ -3,13 +3,6 @@ var phonecatApp = angular.module('phonecatApp', []);
 
 // Define the `PhoneListController` controller on the `phonecatApp` module
 phonecatApp.controller('PhoneListController', function PhoneListController($scope) {
-  var navigatorClass = window.svelteComponents.Navigator;
 
-  $scope.navigatorComponent = new navigatorClass({
-    target: document.getElementById('navigator'),
-    props: {
-      pages: ['Home', 'About', 'Contact', 'Blog', 'FAQ', 'Client Nodes', 'Catalogues']
-    }
-  });
 
 });
